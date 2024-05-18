cea659d9da7958cf89624aec806bc305602216b4
Jorge Matos <jisapjisap@gmail.com>
2024-05-18 23:04:32 +0200

    textGenerate

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Sptolight"
+import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
 
 const Hero = () => {
@@ -22,6 +23,15 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
+
+          <TextGenerateEffect 
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+          </p>
         </div>
       </div>
 
